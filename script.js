2267c549eaf9d5b6841f9b97c8743415138697b5
Marine <112578481+marineExoyan91@users.noreply.github.com>
2023-04-15 21:37:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+AOS.init();
+
+
+function f(){
+	rezyume_block.style.display = "block";
+}
+
+function c(){
+	rezyume_block.style.display = "none";
+}
+
+function q(){
+	share_div.style.display = "block";
+}
+
+function sh(){
+	share_div.style.display = "block";
+}
+
+function a(){
+	share_div.style.display = "none";
+}
+
+function x(){
+	pop.style.display = "none";
+}
+
+function d(){
+	pop.style.display = "block";
+}
+
+function k(){
+	pop.style.display = "block";
+}
+
+
+function n(x){
+	let y = document.getElementsByTagName("a")
+	for (var i = 0; i < y.length; i++){
+		y[i].style.color = "white";
+	}
+	x.style.color = "#AA988A";
+}
+
